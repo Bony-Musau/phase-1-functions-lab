4c2e2416f39444b0c87be218c4f38ed482248c64
bony_musau <boniface.musau@moringaschool.com>
2023-10-09 00:13:08 +0300

    function lab assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+   const hq = 42; 
+   const distance = Math.abs(pick - hq);
+   return distance;
+ }
+ function distanceFromHqInFeet(pick) {
+   const blocks = distanceFromHqInBlocks(pick);
+   const feet = 264;
+   return blocks * feet;
+ }
+
+ function distanceTravelledInFeet(start, end) {
+   const blocksperfeet = Math.abs(end - start);
+   const feetT = 264;
+   return blocksperfeet * feetT;
+ }
+
+ const pick = 50;
+ const destinationBlock = 38;
+ const blocks = distanceFromHqInBlocks(pick);
+ const feet = distanceFromHqInFeet(pick);
+ const feetT = distanceTravelledInFeet(pick, destinationBlock);
+ console.log(blocks); 
+ console.log(`Blocks away: ${blocks}`);
+ console.log(`Feet away: ${feet}`);
+ console.log(`Feet t: ${feetT}`);
+
